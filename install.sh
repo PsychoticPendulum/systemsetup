613263f1f53a8d0fafcb86bdab1937d3fac5ff3b
psychicpenguin <lukas.mairock@gmail.com>
2022-07-22 16:38:45 +0200

    minor bug fixes

--- install.sh
+++ install.sh
@@ -2,15 +2,23 @@
 
 cd $HOME
 
-# Installing basic packages
-sudo pacman -S xorg xorg-xinit i3 adobe-source-code-pro-fonts vim fish ranger neofetch kitty git ttf-font-awesome htop bpytop cmatrix asciiquarium bat base-devel lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan openssh openvpn wget python-pip nasm w3m qbittorrent firefox awesome-terminal-fonts arandr sof-firmware rofi unzip pcmanfm lxappearance gimp code qbittorrent sdl2 sdl2_ttf ncdu veracrypt jdk11-openjdk libreoffice simplescreenrecorder speedtest-cli tmux youtube-dl sl figet cowsay xfce4-screenshooter xfce4-appfinder
+sudo pacman -S cowsay
 
-# Installing python libraries
+function feed () {
+	cowsay -f tux $1
+}
+
+feed 'Installing base packages'
+sudo pacman -S xorg xorg-xinit i3 adobe-source-code-pro-fonts vim fish ranger neofetch kitty git ttf-font-awesome htop bpytop cmatrix asciiquarium bat base-devel lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan openssh openvpn wget python-pip nasm w3m qbittorrent firefox awesome-terminal-fonts arandr sof-firmware rofi unzip pcmanfm lxappearance gimp code qbittorrent sdl2 sdl2_ttf ncdu veracrypt jdk11-openjdk libreoffice simplescreenrecorder speedtest-cli tmux youtube-dl sl figlet xfce4-screenshooter xfce4-appfinder
+
+
+feed 'Installing Python Libraries'
 pip3 install pygame
 pip3 install requests
 pip3 install numpy
 
-# Setting up configuration
+
+feed 'Setting up configuration'
 git clone https://github.com/psychoticpendulum/dotfiles
 cp -Rfv dotfiles/.* dotfiles/* .config/
 rm -Rfv dotfiles
@@ -18,38 +26,48 @@ rm .bashrc
 ln .config/bashrc .bashrc
 ln .config/vimrc .vimrc
 
-# Setting up home
+
+feed 'Setting up home'
 git clone https://github.com/psychoticpendulum/scripts
 git clone https://github.com/psychoticpendulum/coreutils
 git clone https://github.com/psychoticpendulum/unilog
 mkdir Developer
 mv scripts Developer/.scripts
 mv coreutils Developer/CoreUtils
+sudo cp Developer/CoreUtils/sep/sep /bin/sep
 mv unilog Developer/unilog
-Developer/unilog/install.sh
+sh Developer/unilog/install.sh
 mkdir Wallpapers
 
-# Installing yay
+
+feed 'Installing yay'
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-bin
 sudo chown $USER:$USER yay-bin
 cd yay-bin
 makepkg -si
 
-# Installing basic yay packages
-yay -S polybar sweet-folder-icons-git sweet-cursor-theme-git sweet-folder-icons-git
 
-# Handcrafting some configs
+feed 'Installing basic yay packages'
+yay -S polybar sweet-cursor-theme-git sweet-folders-icons-git
+
+
+feed 'Handcrafting some configs'
 sudo vim /etc/default/grub
 sudo vim /etc/lightdm/lightdm.conf
 sudo vim /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo systemctl enable lightdm
 
-# Setting up VPN
+
+feed 'Setting up VPN'
 cd /etc/openvpn
 sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
 sudo unzip ovpn.zip
 sudo rm ovpn.zip
 
+
 cd $HOME
 Develpoper/.scripts/clean.py
+
+
+feed 'We are done' | lolcat

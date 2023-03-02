4829e8e2e72afcf9e04516fef1bf10f99b77e125
PsychoticPendulum <80325726+PsychoticPendulum@users.noreply.github.com>
2023-03-02 11:24:57 +0100

    Update debinstall.sh

--- debinstall.sh
+++ debinstall.sh
@@ -1,39 +1,96 @@
 #! /usr/bin/bash
 
-Header () {
-	printf "\x1B[32m$1\x1B[0m\n"
+Info () {
+    printf "\x1B[1;7m[INFO]\x1B[0m\t$1\n"
 }
 
-Header "Checking permissions ..."
-if [[ "$(whoami)" == "root" ]]; then
-	printf "\x1B[31mScript must not be run as root\x1B[0m\n"
-	exit 1
+Success () {
+    printf "\x1B[1;32;7m [OK] \x1B[0m\t$1\n"
+}
+
+Fail () {
+    printf "\x1B[1;31;7m[FAIL]\x1B[0m\t$1\n"
+	if [[ -z $2 ]]; then
+		exit 1
+	fi
+}
+
+Warning () {
+    printf "\x1B[1;31m$1\x1B[0m\n"
+}
+
+Message () {
+    printf "\x1B[1;35;7m$1\x1B[0m\n"
+}
+
+
+Status () {
+	if [[ $? -eq 0 ]]; then
+		Success "$1"
+	else
+		Fail "$2"
+	fi
+}
+
+Info "Checking Permissions ..."
+if [[ $UID != 0 ]]; then
+	Fail "Script must be run as root"
+fi
+Success "Permissions are ok!"
+
+local_user="root"
+while true; do
+	Info "Searching profiles ..."
+	ls /home | cat -b
+	printf "Enter number of \x1B[1;4mlocal\x1B[0m profile: "
+	read answer
+	if [[ ! -z $answer ]]; then
+		local_user=$(ls /home | cat | head -n $answer | tail -n 1)
+		printf "Old Profile: \x1B[36m$local_user\x1B[0m\nIs that right? [y/N] "
+		read answer
+		if [[ $answer == 'Y' ]] || [[ $answer == 'y' ]]; then
+			Info "Selected User: \x1B[1;4m$local_user\x1B[0m"
+			break
+		fi
+	fi
+done
+
+Info "Getting system information ..."
+distro=$(lsb_release -d | awk '{print $2}')
+Info "Detected distro: \x1B[1;4m$distro\x1B[0m"
+ls /usr/bin/ | grep "apt"  > /dev/null
+Status "Your OS is supported" "Your OS is not supported"
+
+echo -n "Do you want to update the Operating System? [y/N] "
+read answer
+if [[ $answer = 'Y' ]] || [[ $answer = 'y' ]]; then
+	Info "Updating Operating System ..."
+	apt update
+	apt upgrade
+	Status "Updates installed successfully" "Unable to install updates"
 fi
 
-Header "Updating system ..."
-sudo apt update
-sudo apt upgrade
-sudo apt autoremove
+Info "Installing packages ..."
+apt install tmux kitty i3 htop fish vim dmenu git openssh-client xfce4-screenshooter curl
+Status "Packages installed" "Unable to install packages"
 
-Header "Installing packages ..."
-sudo apt install exa tmux htop ranger neofetch vim net-tools build-essential fish htop cmatrix bat wget git curl ncdu cron sl
+Info "Configuring system ..."
+mkdir -p /home/$local_user/.config/i3
+curl -s https://raw.githubusercontent.com/PsychoticPendulum/dotfiles/main/i3/config_default > /home/$local_user/.config/i3/config
+Status "i3 config" "i3 config"
+curl -s https://raw.githubusercontent.com/PsychoticPendulum/dotfiles/main/vimrc > /home/$local_user/.vimrc
+Status "vimrc" "vimrc"
+curl -s https://raw.githubusercontent.com/PsychoticPendulum/dotfiles/main/bashrc > /home/$local_user/.bashrc
+Status "bashrc" "bashrc"
+Status "Configuration successful" "Unable to set up configuration"
 
-Header "Installing configurations ..."
-git clone https://github.com/psychoticpendulum/dotfiles
-cd dotfiles
-cp -Rfv * .* ~/.config
-mv vimrc ~/.vimrc
-mv bashrc ~/.bashrc
-cd ..
-rm -Rfv dotfiles
+Info "Downloading basic scripts ..."
 git clone https://github.com/psychoticpendulum/scripts
-mkdir ~/Developer
-cp -Rfv scripts ~/Developer/.scripts
+mv -fv scripts/*.* /home/$local_user/.scripts/
+chown -R $local_user:$local_user /home/$local_user/.scripts/*
 rm -Rfv scripts
 
-Header "Cleaning up ..."
-cd ~/
-rmdir *
-
-source ~/.bashrc
-Header "Done!"
+Info "Enabling SSH Service"
+systemctl start ssh
+systemctl enable ssh
+Status "SSH is active" "Unable to enable or start SSH"
